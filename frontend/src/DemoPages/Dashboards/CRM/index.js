caa539dfc20ba34a05e8719790aee3400031c7ee
MikkoDC <mikkodecastro@ymail.com>
2019-07-26 00:16:54 +0400

    Initial Commit

---
+++ frontend/src/DemoPages/Dashboards/CRM/index.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+// Examples
+import CRMDashboard1 from "./Examples/Variation1";
+
+export default class CRMDashboard extends Component {
+  render() {
+    return (
+      <Fragment>
+        <CRMDashboard1 />
+      </Fragment>
+    );
+  }
+}
